e18e9052e61f2f4a6f8e1441f60a53d83e75cd4d
Sarah Fullerton <sjanesasha@gmail.com>
2024-02-29 23:19:59 -0800

    update files pls work

--- sql/07b.sql
+++ sql/07b.sql
@@ -3,17 +3,17 @@
  * but instead of using the NOT IN operator, you are to use a LEFT JOIN.
  */
 
-
 select distinct film.title from film
 JOIN inventory USING (film_id)
 LEFT JOIN (
-    select inventory.film_id from inventory
+    select distinct film_id from film
+    JOIN inventory USING (film_id)
     JOIN rental USING (inventory_id)
     JOIN customer USING (customer_id)
     JOIN address USING (address_id)
     JOIN city USING (city_id)
-    JOIN country USING (country_id) WHERE country = 'United States'
-) USING (film_id)
+    JOIN country USING (country_id) WHERE country ILIKE 'United States'
+) t USING (film_id)
 where country is NULL
 order by film.title;
 
