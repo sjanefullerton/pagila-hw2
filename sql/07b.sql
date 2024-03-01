3a0676909d6704d3f5bbbe840f431089f0742242
Sarah Fullerton <sjanesasha@gmail.com>
2024-02-29 23:25:06 -0800

    update files pls work

--- sql/07b.sql
+++ sql/07b.sql
@@ -6,14 +6,14 @@
 select distinct film.title from film
 JOIN inventory USING (film_id)
 LEFT JOIN (
-    select distinct film_id from film
+    select distinct country, film_id from film
     JOIN inventory USING (film_id)
     JOIN rental USING (inventory_id)
     JOIN customer USING (customer_id)
     JOIN address USING (address_id)
     JOIN city USING (city_id)
-    JOIN country USING (country_id) WHERE country ILIKE 'United States'
+    JOIN country USING (country_id) where country ilike 'United States'
 ) t USING (film_id)
-where country.country is NULL
+where country is NULL
 order by film.title;
 
