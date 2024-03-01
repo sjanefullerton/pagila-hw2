f19222b260f1fe8ecd794b0701ee23b2acb29a36
Sarah Fullerton <sjanesasha@gmail.com>
2024-02-29 19:46:52 -0800

    update

--- sql/05b.sql
+++ sql/05b.sql
@@ -2,4 +2,7 @@
  * This problem is the same as 05.sql,
  * but instead of using the NOT IN operator, you are to use a LEFT JOIN.
  */
-
+select last_name, first_name FROM actor
+LEFT JOIN customer USING (first_name, last_name)
+WHERE customer_id is NULL
+ORDER BY last_name, first_name
--- sql/06b.sql
+++ sql/06b.sql
@@ -2,3 +2,6 @@
  * This problem is the same as 06.sql,
  * but instead of using the NOT IN operator, you are to use a LEFT JOIN.
  */
+
+
+select title from film LEFT JOIN inventory USING (film_id) where inventory.film_id is NULL ORDER BY title;
