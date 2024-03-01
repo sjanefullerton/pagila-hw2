d0787e772b552712eab0d5a10cd8469158f09a5c
Sarah Fullerton <sjanesasha@gmail.com>
2024-02-29 20:00:24 -0800

    update

--- sql/08.sql
+++ sql/08.sql
@@ -6,5 +6,7 @@
  * Use `unnest(special_features)` in a subquery.
  */
 
-select title from (select title, rating, unnest(special_features) as feature from film) where rating = 'G' and feature = 'Trailers'
-ORDER BY title ASC;
+select title from (select title, rating, unnest(special_features) as feature from film) 
+where rating = 'G' and feature = 'Trailers'
+order by title;
+
