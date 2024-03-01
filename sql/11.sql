a54b820f319a417209ef2354c4e7c94207858ace
Sarah Fullerton <sjanesasha@gmail.com>
2024-02-29 22:21:10 -0800

    update files pls work

--- sql/11.sql
+++ sql/11.sql
@@ -5,6 +5,6 @@
 select firstname || ' ' || last_name AS "Actor Name" from actor
 JOIN film_actor USING (actor_id)
 JOIN film USING (film_id)
-where ANY (film.special_features) = 'Behind the Scenes'
+where 'Behind the Scenes' = ANY (film.special_features) 
 order by "Actor Name";
 
